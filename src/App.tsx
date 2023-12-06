71fe3e7aaa41ea8a1ca388bf2f6f6fc6f665b031
dnlklmn <pelicansinberlin@gmail.com>
2023-12-06 17:07:02 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="flex h-16 items-center justify-between p-6">
+        <span className="text-foreground-contrast">DANIEL KALMAN</span>
+        <span>ABOUT</span>
+      </div>
+      <div className="bg-background-default h-12 w-12">ham ham</div>
+    </>
+  );
+}
+
+export default App;
