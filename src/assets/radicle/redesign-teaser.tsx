019a212ffbadf4851c8eae796a794837990e45af
dnlklmn <pelicansinberlin@gmail.com>
2023-12-15 16:15:07 +0100

    Move overlay with hooks

--- src/assets/radicle/redesign-teaser.tsx
+++ src/assets/radicle/redesign-teaser.tsx
@@ -1,55 +1,52 @@
 import { Link } from "react-router-dom";
 import Home from "./Home.png";
-
-let x: number;
-let y: number;
-let entryX: number;
-let entryY: number;
-
-const imageContainer: HTMLElement | null = document.getElementById("image-container");
+import { useState } from "react";
 
 export default function RadicleRedesignTeaser({ linkTo }: { linkTo: string }) {
-  function registerEntry(event: React.MouseEvent<HTMLDivElement>) {
-    // Get position of the cursor within the div.
-    entryX = event.clientX;
-    entryY = event.clientY;
-    console.log("registerEntry: ", entryX, entryY);
-  }
+  const [entry, setEntry] = useState({ x: 0, y: 0 });
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [mouseOver, setMouseOver] = useState(false);
 
-  function followCursor(event: React.MouseEvent<HTMLDivElement>) {
-    event.preventDefault();
-    x = event.clientX - entryX; // X position within the div.
-    y = event.clientY - entryY; // Y position within the div.}
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    setPosition({
+      x: e.nativeEvent.clientX,
+      y: e.nativeEvent.clientY,
+    });
+    console.log("x: ", e.nativeEvent.clientX, "y: ", e.nativeEvent.clientY);
+  };
 
-    if (imageContainer) {
-      imageContainer.style.transition = "all 0s ease";
-      imageContainer.style.left = `${-8 + x / 72}px`;
-      imageContainer.style.top = `${-8 + y / 72}px`;
-      console.log("followCursor: ", x, y);
-    }
-  }
+  const handleMouseEnter = (e: React.MouseEvent<HTMLDivElement>) => {
+    setEntry({ x: e.nativeEvent.clientX, y: e.nativeEvent.clientY }),
+      console.log("enter-x: ", e.nativeEvent.clientX, "enter-y: ", e.nativeEvent.clientY);
+    setMouseOver(true);
+  };
 
-  function returnToStart() {
-    if (imageContainer) {
-      imageContainer.style.transition = "all 0.5s ease";
-      imageContainer.style.left = `-8px`;
-      imageContainer.style.top = `-8px`;
-      console.log("returnToStart");
-    }
-  }
-
+  const handleMouseLeave = () => {
+    setPosition({ x: 0, y: 0 });
+    setEntry({ x: 0, y: 0 });
+    setMouseOver(false);
+  };
   return (
-    <div id="tracking-div" className="relative h-fit w-1/2">
+    <div
+      onMouseMove={handleMouseMove}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      id="tracking-div"
+      className="relative h-fit w-1/2 "
+    >
       <Link to={linkTo}>
         <>
           <img src={Home} />
           <div
-            style={{ left: x }}
+            style={{
+              left: `${-8 + (entry.x - position.x) / 72}px`,
+              top: `${-8 + (entry.y - position.y) / 72}px`,
+              pointerEvents: "none",
+            }}
             id="image-container"
-            className="trasnsition-all absolute -left-2 -top-2 flex h-full w-full flex-col gap-2"
-            onPointerMove={followCursor}
-            onMouseOut={returnToStart}
-            onMouseEnter={registerEntry}
+            className={`absolute flex h-full w-full flex-col gap-2 ${
+              mouseOver ? "transition-none" : "trasnsition-all duration-300 ease-out"
+            }`}
           >
             <div className="border-foreground-contrast h-fit w-full border-4 p-1">
               <p className="bg-foreground-contrast h-fit w-fit p-1 italic">App</p>
@@ -63,6 +60,7 @@ export default function RadicleRedesignTeaser({ linkTo }: { linkTo: string }) {
               </div>
             </div>
           </div>
+          <script></script>
         </>
       </Link>
     </div>
